747b50325e3b90baa515338a9f71933e8a83445c
Ariadna Dielingen <72440018+AriadnaDieLo@users.noreply.github.com>
2020-11-02 07:52:35 +0000

    Create data_types.cc

---
+++ data_types.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    std::cout << "El tipo de datos char se representa utilizando "
+    << sizeof(char) << " bytes." << endl;
+    std::cout << "El tipo de datos bool se representa utilizando "
+    << sizeof(bool) << " bytes." << endl;
+    std::cout << "El tipo de datos int se representa utilizando " 
+    << sizeof(int) << " bytes." << endl;
+    std::cout << "El tipo de datos float se representa utilizando "
+    << sizeof(float) << " bytes." << endl;
+    std::cout << "El tipo de datos double se representa utilizando "
+    << sizeof(double) << " bytes." << endl;
+    
+    
+}
